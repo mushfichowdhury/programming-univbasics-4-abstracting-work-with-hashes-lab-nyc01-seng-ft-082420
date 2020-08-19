cde204cb401f0ff5ea4c5e8adf0326cbace0db0e
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-19 07:34:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,8 +1,7 @@
 def my_hash_creator(key, value)
   hash = {
-    :key => value 
+    key => value 
   }
-  key = :key
   # return a hash that includes the key and value parameters passed into this method
 end
 
